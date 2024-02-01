23254e8bbe9a56dcf4a47a33f0e207ebd6410b1e
Yasir Mohamed <Yasirwiifto@gmail.com>
2024-02-01 17:01:56 +0000

    added content inside allow hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4+u4c8+$fla_h1&3)92-fzbsvbrd$frr*=0nwty!^hd^i3q)4u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
