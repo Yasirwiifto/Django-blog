ad6a405ccc04d8e0cefab705f8c7e434e4b8d727
Yasir Mohamed <Yasirwiifto@gmail.com>
2024-02-27 12:54:38 +0000

    updated

---
+++ blog/about/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def about_me(request):
+    return HttpResponse('Ya Fatah make Palestine Free Free Free')
+
+    
--- blog/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def my_blog(request):
-    return HttpResponse("Hello, Blog!")
